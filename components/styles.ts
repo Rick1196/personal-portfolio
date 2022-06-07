b42a294a2e54cc3bde796096e29ee62bc193bdd5
Rick1196 <ricardompp11@gmail.com>
2022-06-06 22:38:03 -0500

    navbar styles

--- components/styles.ts
+++ components/styles.ts
@@ -1,7 +1,19 @@
 import React, { FC, useState, MouseEvent } from "react";
-import { Typography } from "@mui/material";
+import { Menu, MenuItem, Typography } from "@mui/material";
 import styled from "styled-components";
 
 export const CustomTypography = styled(Typography)(({ theme }) => ({
   color: `${theme.theme.text} !important`,
+}));
+
+export const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
+  color: `${theme.theme.text} !important`,
+  backgroundColor: `${theme.theme.body} !important`,
+}));
+
+export const CustomMenu = styled(Menu)(({ theme }) => ({
+  color: `${theme.theme.text} !important`,
+  '.MuiMenu-list': {
+    backgroundColor: `${theme.theme.body} !important`,
+  },
 }));
